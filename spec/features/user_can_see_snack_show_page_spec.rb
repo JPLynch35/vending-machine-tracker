6d17c8b66c5f026f6a5514f5a4401f9258a89d4e
JP Lynch <JPLynch35@gmail.com>
2018-07-22 17:46:05 -0600

    change setup parameters for snack show test

--- spec/features/user_can_see_snack_show_page_spec.rb
+++ spec/features/user_can_see_snack_show_page_spec.rb
@@ -8,15 +8,12 @@ feature 'When a user visits a snack show page' do
     snack1 = dons1.snacks.create(name: 'Doritos', price: 150)
     snack2 = dons1.snacks.create(name: 'Cookies', price: 250)
     snack3 = dons1.snacks.create(name: 'Peanuts', price: 100)
-    snack4 = dons2.snacks.create(name: 'Doritos', price: 150)
-    snack5 = dons2.snacks.create(name: 'Cookies', price: 250)
-    snack6 = dons2.snacks.create(name: 'Peanuts', price: 100)
-    snack7 = dons2.snacks.create(name: 'Gum', price: 120)
+    MachineSnack.create(snack_id: snack1.id, machine_id: dons2.id)
 
     visit snack_path(snack1)
     
     expect(page).to have_content(snack1.name)
-    expect(page).to have_content("Price: $#{snack1.price}")
+    expect(page).to have_content("Price: $1.5")
     expect(page).to have_content("#{dons1.location}")
     expect(page).to have_content("Average Snack Price: $1.67")
     expect(page).to have_content("Number of Snacks: #{dons1.snacks.count}")
